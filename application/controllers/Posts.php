168d1d00d07f60ae56b2da990020f769330ad64a
SpriteN <MacBook_Pro_Rretina_512Gb@MacBooks-MacBook-Pro.local>
2017-04-27 17:28:39 +0300

    Skelbimu atvaizdvimas
    atvaizduojami skelbimai is duomenu bazes

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -3,7 +3,7 @@
     public function index(){
       $data['title'] = 'posts';
       $data['posts'] = $this->post_model->get_posts();
-      print_r($data['posts']);
+      //print_r($data['posts']);
       $this->load->view('templates/header');
       $this->load->view('posts/index',$data);
       $this->load->view('templates/footer');
--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,1 +1,10 @@
-<h2><?= $title ?></h2>
+<h2>Skelbimai</h2>
+<table class="table table-stripped">
+<?php foreach ($posts as $post) : ?>
+  <tr>
+    <th><?php echo $post['pavadinimas']; ?></th>
+    <th><?php echo $post['kategorija']; ?></th>
+    <th><?php echo $post['subkategorija']; ?></th>
+  </tr>
+<?php endforeach; ?>
+</table>
